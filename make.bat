186980331ad029cdf2ae3cf31343072ebe2d6425
shirouzu <shirouzu@ipmsg.org>
2020-03-06 13:49:22 +0900

    non-sensitive selection

---
+++ make.bat
@@ -0,0 +1,4 @@
+"C:\tools\vs2019\VC\Tools\MSVC\14.24.28314\bin\Hostx64\x64\nmake.exe" -f makefile.vc && "C:/Program Files (x86)/Windows Kits/10/App Certification Kit/signtool.exe" sign /a /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /fd sha256 putty.exe
+
+pause
+
